677d92965bc84e140574be2dd0bfff4961f1091b
slayer321 <sachin.maurya@fluxninja.com>
2022-09-29 19:33:45 +0530

    initial code

---
+++ pre_commit_hooks/circleci_validate.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+
+from typing import Sequence
+import argparse
+from pre_commit_hooks import util
+
+
+def installation():
+    util.cmd_output('sudo','apt','install','snapd')
+    util.cmd_output('sudo','snap','install' ,'circleci')
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    args = parser.parse_args(argv)
+
+    installation()
+    retval = 0
+    for filename in args.filenames:
+        try:
+            util.cmd_output('circleci', 'config','validate',*args.filenames)
+        except Exception as e:
+            print(f'{filename}: Failed to validate ({e})')
+            retval = 1
+    return retval
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
