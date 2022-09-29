7a0aa5b3e7b016ed3701ab912b3150e12d2bdde4
Sachin Maurya <57769917+slayer321@users.noreply.github.com>
2022-09-29 19:35:36 +0530

    initial code (#1)

---
+++ pre_commit_hooks/util.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import subprocess
+from typing import Any
+
+
+class CalledProcessError(RuntimeError):
+    pass
+
+def cmd_output(*cmd: str, retcode: int | None = 0, **kwargs: Any) -> str:
+    kwargs.setdefault('stdout', subprocess.PIPE)
+    kwargs.setdefault('stderr', subprocess.PIPE)
+    proc = subprocess.Popen(cmd, **kwargs)
+    stdout, stderr = proc.communicate()
+    stdout = stdout.decode()
+    if retcode is not None and proc.returncode != retcode:
+        raise CalledProcessError(cmd, retcode, proc.returncode, stdout, stderr)
+    return stdout
