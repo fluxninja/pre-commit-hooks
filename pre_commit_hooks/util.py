0a38418c52ceb4ecd65a352ffa0bc3ca330a6d10
slayer321 <sachin.maurya@fluxninja.com>
2022-09-30 16:07:31 +0530

    add check for know errors

--- pre_commit_hooks/util.py
+++ pre_commit_hooks/util.py
@@ -7,12 +7,15 @@ from typing import Any
 class CalledProcessError(RuntimeError):
     pass
 
-def cmd_output(*cmd: str, retcode: int | None = 0, **kwargs: Any) -> str:
+def cmd_output(*cmd: str, retcode: int | None = 0,okcode:int= 0, **kwargs: Any) -> str:
+    """
+    okcode: This parameter acts as the code where you already that you are going to get this error code
+    """
     kwargs.setdefault('stdout', subprocess.PIPE)
     kwargs.setdefault('stderr', subprocess.PIPE)
     proc = subprocess.Popen(cmd, **kwargs)
     stdout, stderr = proc.communicate()
     stdout = stdout.decode()
-    if retcode is not None and proc.returncode != retcode:
+    if retcode is not None and (proc.returncode != retcode or proc.returncode  == okcode ):
         raise CalledProcessError(cmd, retcode, proc.returncode, stdout, stderr)
     return stdout
